71aa0ecfc8572d0e421a634a2bfff1b3ecffecd9
kouhe0115 <k.soccer0115@icloud.com>
2019-04-17 14:25:11 +0900

    edit

--- config/environment.rb
+++ config/environment.rb
@@ -3,3 +3,33 @@ require_relative 'application'
 
 # Initialize the Rails application.
 Rails.application.initialize!
+
+require 'sendgrid-ruby'
+include SendGrid
+
+data = JSON.parse('{
+  "personalizations": [
+    {
+      "to": [
+        {
+          "email": "test@example.com"
+        }
+      ],
+      "subject": "Hello World from the SendGrid Ruby Library!"
+    }
+  ],
+  "from": {
+    "email": "test@example.com"
+  },
+  "content": [
+    {
+      "type": "text/plain",
+      "value": "Hello, Email!"
+    }
+  ]
+}')
+sg = SendGrid::API.new(api_key: ENV['SENDGRID_API_KEY'])
+response = sg.client.mail._("send").post(request_body: data)
+puts response.status_code
+puts response.body
+puts response.headers
